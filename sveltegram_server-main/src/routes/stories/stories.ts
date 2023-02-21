37b2fcb95a868b947c4d9803056111e2b2314587
Alex Ma <66192750+RyukCEO@users.noreply.github.com>
2023-02-20 23:33:03 -0800

    story update

--- sveltegram_server-main/src/routes/stories/stories.ts
+++ sveltegram_server-main/src/routes/stories/stories.ts
@@ -4,6 +4,8 @@ const router = express.Router()
 import mongoose from 'mongoose'
 //const Stories = mongoose.model("Stories")
 import Stories from "../../models/stories";
+import { checkAuth } from "../../middlewares/authenticate";
+
 
 
 router.get('/allstories',(req,res)=>{
@@ -17,15 +19,30 @@ router.get('/allstories',(req,res)=>{
     })
 })
 
+router.get('/:storyId',(req,res)=>{
+
+	const { storyId } = req.params;
+    Stories.findById({
+        _id: storyId
+    })
+    .then(stories =>{
+        res.json({stories})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
 //add requiredLogin
-router.post('/creatstories',(req,res)=>{
-    const {title,body} =req.body
+router.post('/creatstories', checkAuth, (req,res)=>{
+    const {title,body,photo} =req.body
     if(!title || !body){
         return res.status(422).json({error:"enter all the fields"})
     }
     const stories = new Stories({
         title,
         body,
+        photo,
         postedBy: req.user
     })
     stories.save().then(result=>{
